875a0afe7441e0eb38d675afd0dbd7a1c9de62a5
aramwala <aramwala@ryerson.ca>
2015-10-23 16:46:20 -0400

    Hello world template

---
+++ src/LAB_5.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : LAB_5.cpp
+// Author      : Akshar Ramwala
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
